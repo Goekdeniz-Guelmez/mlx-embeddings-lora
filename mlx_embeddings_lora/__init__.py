b9c8ad5a0446305a0a8c78058a8433d700269750
Gökdeniz Gülmez <60228478+Goekdeniz-Guelmez@users.noreply.github.com>
2025-11-13 13:50:24 +0100

    first working prototype

---
+++ mlx_embeddings_lora/__init__.py
@@ -0,0 +1,4 @@
+from .version import __version__
+import os
+
+os.environ["TRANSFORMERS_NO_ADVISORY_WARNINGS"] = "1"
---
+++ mlx_embeddings_lora/trainer/__init__.py
@@ -0,0 +1,3 @@
+from .utils import linear_to_lora_layers
+
+from .contrastive_trainer import TrainingArgs, evaluate, train
